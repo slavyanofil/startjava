d3e60300fc2515ead1f850df88941f1c32d4b084
Vyacheslav Trufakin <slavyanofil@gmail.com>
2021-09-09 21:51:43 +0400

    Formatting correction of Variable.java and ConditionalStatement.java

--- Lesson1/ConditionalStatement.java
+++ Lesson1/ConditionalStatement.java
@@ -1,29 +1,32 @@
 public class ConditionalStatement {   
     public static void main(String[] args) {
-        short age=35;
-        boolean male=true;
-        float height=1.75f;
-        char firstLetterOfName='V';
-        if (age>20) {
+        short age = 35;
+        if (age > 20) {
             System.out.println("Ты уже не молодой :(");
         }
+
+        boolean male = true;
         if (male) {
             System.out.println("Пол мужской");
         }
         if (!male) {
             System.out.println("Корнет, Вы жещина?");
         }
-        if (height<1.80) {
+
+        float height = 1.75f;
+        if (height < 1.80) {
             System.out.println("Рост не большой");
         } else {
             System.out.println("Рост большой");
         }
-        if (firstLetterOfName=='\u004D') {
+        
+        char firstLetterOfName = 'V';
+        if (firstLetterOfName == 'M') {
             System.out.println("имя начинается на M");
-        } else if (firstLetterOfName=='\u0049') {
+        } else if (firstLetterOfName == 'I') {
             System.out.println("имя начинается на I");
         } else {
-            System.out.println("имя начинается ни на М, и ни на I, а на "+firstLetterOfName);
+            System.out.println("имя начинается ни на М, и ни на I, а на " + firstLetterOfName);
         }
     }
 }
--- Lesson1/Variable.java
+++ Lesson1/Variable.java
@@ -1,21 +1,21 @@
 public class Variable {   
     public static void main(String[] args) {
-        byte cpus=6;
-        short pagefileSize=18749;
-        int ramSize=16384;
-        long diskCapacity=976166908;
-        float cpuFreq=2.9f;
-        double connectSpeed=94.8;
-        char cpuFamily='i';
-        boolean personal=true;
+        byte cpus = 6;
+        short pagefileSize = 18749;
+        int ramSize = 16384;
+        long diskCapacity = 976166908;
+        float cpuFreq = 2.9f;
+        double connectSpeed = 94.8;
+        char cpuFamily = 'i';
+        boolean personal = true;
         System.out.println("- PC info -");
-        System.out.println("CPUs "+cpus);
-        System.out.println("Pagefile size "+pagefileSize+"MB");
-        System.out.println("RAM "+ramSize+"MB");
-        System.out.println("Hard disk "+diskCapacity+"KBytes");
-        System.out.println("CPU @ "+cpuFreq+"GHz");
-        System.out.println("Connection speed "+connectSpeed+"MBps");
-        System.out.println("CPU family "+cpuFamily);
-        System.out.println("Is this a personal computer? "+personal);
+        System.out.println("CPUs " + cpus);
+        System.out.println("Pagefile size " + pagefileSize + "MB");
+        System.out.println("RAM " + ramSize + "MB");
+        System.out.println("Hard disk " + diskCapacity + "KBytes");
+        System.out.println("CPU @ " + cpuFreq + "GHz");
+        System.out.println("Connection speed " + connectSpeed + "MBps");
+        System.out.println("CPU family " + cpuFamily);
+        System.out.println("Is this a personal computer? " + personal);
     }
 }
