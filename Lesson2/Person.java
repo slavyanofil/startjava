55c2528936afc535ded49b38149ff2cb2c6ed1e4
Vyacheslav Trufakin <slavyanofil@gmail.com>
2021-09-17 17:51:04 +0400

    Adding Lesson2/Person.java

---
+++ Lesson2/Person.java
@@ -0,0 +1,25 @@
+public class Person {   
+    boolean male = true;
+    String name = "David";
+    float height = 170.5f;
+    float weight = 70.5f;
+    int age = 50;
+
+    public Person() {
+    }
+
+    public void move() {
+    }
+
+    public void sit() {
+    }
+
+    public void run() {
+    }
+
+    public void talk() {
+    }
+
+    public void learnJava() {
+    }
+}
