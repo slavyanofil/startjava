0d241b73b90554415a348ed155011742edccc2a6
Vyacheslav Trufakin <slavyanofil@gmail.com>
2021-09-18 15:47:25 +0400

    Wolf.java and WolfTest.java modification (ex. 2.5)

--- Lesson2/Wolf.java
+++ Lesson2/Wolf.java
@@ -1,9 +1,53 @@
 public class Wolf {   
-    boolean male = true;
-    String name = "Grey";
-    float weight = 50.5f;
-    int age = 5;
-    String colour = "Dark grey";
+    private boolean male = true;
+    private String name = "Grey";
+    private float weight = 50.5f;
+    private int age = 5;
+    private String colour = "Dark grey";
+
+    public boolean getMale() {
+        return male;
+    }
+
+    public void setMale(boolean male) {
+        this.male = male;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
 
     public void move() {
         System.out.println("Волк идёт");
--- Lesson2/WolfTest.java
+++ Lesson2/WolfTest.java
@@ -1,14 +1,16 @@
 public class WolfTest {   
     public static void main(String[] args) {
         Wolf wolf = new Wolf();
-        wolf.male = true;
-        wolf.name = "Akella";
-        wolf.colour = "Light grey";
-        System.out.println("Волк самец: " + wolf.male);
-        System.out.println("Кличка волка: " + wolf.name);
-        System.out.println("Вес волка: " + wolf.weight);
-        System.out.println("Возраст волка: " + wolf.age);
-        System.out.println("Окрас: " + wolf.colour);
+        wolf.setMale(true);
+        wolf.setName("Akella");
+        wolf.setWeight(43.5f);
+        wolf.setAge(15);
+        wolf.setColour("Light grey");
+        System.out.println("Волк самец: " + wolf.getMale());
+        System.out.println("Кличка волка: " + wolf.getName());
+        System.out.println("Вес волка: " + wolf.getWeight());
+        System.out.println("Возраст волка: " + wolf.getAge());
+        System.out.println("Окрас: " + wolf.getColour());
         wolf.move();
         wolf.sit();
         wolf.run();
