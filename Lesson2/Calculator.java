5b0aa1c87808de736644b1f374e4d821cb531886
Vyacheslav Trufakin <slavyanofil@gmail.com>
2021-09-25 18:52:46 +0400

    Calculator.java updating

--- Lesson2/Calculator.java
+++ Lesson2/Calculator.java
@@ -1,52 +1,52 @@
 public class Calculator {
-        private int number1;
-        private int number2;
-        private char operation;
+    private int number1;
+    private int number2;
+    private char operation;
 
-        public void setNumber1(int number1) {
-            this.number1 = number1;
-        }
+    public void setNumber1(int number1) {
+        this.number1 = number1;
+    }
 
-        public void setNumber2(int number2) {
-            this.number2 = number2;
-        }
+    public void setNumber2(int number2) {
+        this.number2 = number2;
+    }
 
-        public void setOperation(char operation) {
-            this.operation = operation;
-        }
+    public void setOperation(char operation) {
+        this.operation = operation;
+    }
 
-        public void calculate() {
-            double result = 0;
-            if (number1 > 0 && number2 > 0) {
-                switch (operation) {
-                    case '+': 
-                        result = number1 + number2;
-                        break;
-                    case '-': 
-                        result = number1 - number2;
-                        break;
-                    case '*': 
-                        result = number1 * number2;
-                        break;
-                    case '/': 
-                        result = (double) number1 / number2;
-                        break;
-                    case '^': 
-                        result = 1;
-                        System.out.print(number1 + " ^ " + number2 +" = ");
-                        for (int i = 0; i < number2; i++) {
-                            result *= number1;
-                        }
-                        System.out.println(result);
-                        break;
-                    case '%': 
-                        result = number1 % number2;
-                        break;
-                    default: System.out.println("Недопустимая операция");
-                }
-            } else {
-                System.out.println("Числа должны быть положительными");
+    public void calculate() {
+        double result = 0;
+        if (number1 > 0 && number2 > 0) {
+            switch (operation) {
+                case '+': 
+                    result = number1 + number2;
+                    break;
+                case '-': 
+                    result = number1 - number2;
+                    break;
+                case '*': 
+                    result = number1 * number2;
+                    break;
+                case '/': 
+                    result = (double) number1 / number2;
+                    break;
+                case '^': 
+                    result = 1;
+                    System.out.print(number1 + " ^ " + number2 +" = ");
+                    for (int i = 0; i < number2; i++) {
+                        result *= number1;
+                    }
+                    System.out.println(result);
+                    break;
+                case '%': 
+                    result = number1 % number2;
+                    break;
+                default: System.out.println("Недопустимая операция");
             }
-            System.out.println("" + number1 + operation + number2 + " = " + result);
+        } else {
+            System.out.println("Числа должны быть положительными");
         }
+        System.out.println("Результат операции: " + number1 + operation + number2 + " = " + result);
+    }
 }
