1fc0694b30ecc78aaa3cd1b12bb1f7c357f3f224
Vyacheslav Trufakin <slavyanofil@gmail.com>
2021-10-23 21:08:15 +0400

    Lesson 5 HW (PostgreSQL)

---
+++ src/com/startjava/lesson_5/create_db.sql
@@ -0,0 +1,14 @@
+-- create database
+
+CREATE TABLE Jaegers
+(
+    id        SERIAL PRIMARY KEY,
+    modelName TEXT,
+    mark      TEXT,
+    height    REAL,
+    weight    INTEGER,
+    status    TEXT,
+    origin    TEXT,
+    launch    DATE,
+    kaijuKill INTEGER
+);
---
+++ src/com/startjava/lesson_5/init_db.sql
@@ -0,0 +1,13 @@
+-- init database
+
+INSERT INTO Jaegers (modelName, mark, height, weight, status, origin, launch, kaijuKill)
+VALUES ('Gipsy Danger', 'Mark-3', '79.25', '1980', 'Destroyed', 'USA', 'July 10, 2017', '9'),
+       ('Striker Eureka', 'Mark-5', '76.2', '1850', 'Destroyed', 'Australia', 'November 2, 2019', '11'),
+       ('Cherno Alpha', 'Mark-1', '85.34', '2412', 'Destroyed', 'Russia', 'January 1, 2015', '6'),
+       ('Gipsy Avenger', 'Mark-6', '81.77', '2004', 'Destroyed', 'USA', 'January 12, 2034', '1'),
+       ('Crimson Typhoon', 'Mark-4', '76.2', '1722', 'Destroyed', 'China', 'August 22, 2018', '7'),
+       ('Coyote Tango', 'Mark-1', '85.34', '2312', 'Destroyed', 'Japan', 'December 30, 2015', '2'),
+       ('Horizon Brave', 'Mark-1', '72.54', '7890', 'Destroyed', 'China', 'December 22, 2015', '2'),
+       ('Diablo Intercept', 'Mark-2', '77.5', '2650', 'Undestroyed', 'Chile', 'August 2, 2016', '1'),
+       ('Mammoth Apostle', 'Mark-4', '80.1', '3400', 'Destroyed', 'USA', 'November 19, 2018', '2'),
+       ('Tacit Ronin', 'Mark-1', '74.37', '7450', 'Destroyed', 'Japan', 'December 6, 2015', '3');
